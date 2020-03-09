07fb816bb4851a98d85f93000d6999d824b0224a
Suraj Madaan <sumadaan@in.ibm.com>
2020-03-09 15:35:57 +0530

    Calculator added

---
+++ calc.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    "Add Fucntion"
+    return x+y
+
+def subtract(x,y):
+    "Sub Fucntion"
+    return x-y
+
+def multiply(x,y):
+    "Mul Fucntion"
+    return x*y
+
+def divide(x,y):
+    "Div Funciton"
+    return x /y
+
